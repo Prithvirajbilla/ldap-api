6b114d3e39e05b3108743061ce540d6998c42548
PrithvirajBilla <prithviraj.m.billa@gmail.com>
2013-07-27 14:42:34 +0530

    creating a json api for ldap iitb

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT']."/ldap-api/ldap-auth.php";
+    // indenting JSON
+    //
+    function indent($json) {
+
+    $result      = '';
+    $pos         = 0;
+    $strLen      = strlen($json);
+    $indentStr   = '  ';
+    $newLine     = "\n";
+    $prevChar    = '';
+    $outOfQuotes = true;
+
+    for ($i=0; $i<=$strLen; $i++) {
+
+        // Grab the next character in the string.
+        $char = substr($json, $i, 1);
+
+        // Are we inside a quoted string?
+        if ($char == '"' && $prevChar != '\\') {
+            $outOfQuotes = !$outOfQuotes;
+
+        // If this character is the end of an element,
+        // output a new line and indent the next line.
+        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+            $result .= $newLine;
+            $pos --;
+            for ($j=0; $j<$pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+
+        // Add the character to the result string.
+        $result .= $char;
+
+        // If the last character was the beginning of an element,
+        // output a new line and indent the next line.
+        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+            $result .= $newLine;
+            if ($char == '{' || $char == '[') {
+                $pos ++;
+            }
+
+            for ($j = 0; $j < $pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+
+        $prevChar = $char;
+    }
+
+    return $result;
+}
+
+if(isset($_GET["user"]))
+{
+	$id = $_GET["user"];
+	$enti = new ldapAuth($id);
+	$res = $enti->getInfo();
+	$fname = $enti->getFirstName();
+	$lname = $enti->getLastName();
+	$rollno = $enti->getRollNo();
+	$mail = $enti->getMail();
+	$dept = $enti->getDept();
+	if($fname != null)
+	{
+		$named_array = array(
+			"ldapid" => $id,
+			"fname" => $fname,
+			"lname" => $lname,
+			"rollno" => $rollno,
+			"mail" => $mail,
+			"dept" => $dept,
+	    );
+		header('Content-type: text/json');
+	    echo indent(json_encode($named_array));
+	}
+	else
+	{
+		$named_array = array(
+			"error" => "user doesn't exist", );
+		header('Content-type: text/json');
+		echo indent(json_encode($named_array));
+	}
+}
+else
+{
+		$named_array = array(
+		"error" => "Incorrect Usage", );
+		header('Content-type: text/json');
+		echo indent(json_encode($named_array));
+
+}
+?>
---
+++ ldap-auth.php
@@ -0,0 +1,131 @@
+<?php
+function getDept( $homedirectory ){
+    if (empty($homedirectory)) {
+    return '';
+}
+    $anotherArray = (explode("/", strrev($homedirectory)));
+    return strrev($anotherArray[1]);
+}
+class ldapAuth
+{
+	// LDAP settings
+    private  $ldap_host = 'ldap.iitb.ac.in';
+    private  $ldap_port = 389;
+    private  $baseDN = 'dc=iitb,dc=ac,dc=in';
+    private $ldap_id;
+    private $pass;
+    private $ldap_conn;
+    private $search_result;
+    private $info;
+    public function __construct()
+    {
+   		$arguments = func_get_args(); 
+
+	   switch(sizeof(func_get_args()))      
+	   {
+	    case 0: 
+	        break; 
+	    case 1: //One argument
+	        $this->ldap_id = $arguments[0]; 
+	        break;              
+	    case 2:  //Two arguments
+	        $this->ldap_id = $arguments[0];
+	        $this->pass = $arguments[1]; 
+	        break;            
+	   }
+	   $this->ldap_conn = ldap_connect($this->ldap_host, $this->ldap_port);
+       ldap_set_option($this->ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+       $this->search_result = ldap_search($this->ldap_conn, $this->baseDN, 'uid=' . $this->ldap_id, array('dn', 'givenname', 'sn','employeenumber', 'mail', 'homedirectory'));
+       $this->info = ldap_get_entries($this->ldap_conn, $this->search_result);
+    }
+    public function getFirstName()
+    {
+    	if (!empty($this->search_result))
+    	{
+    		$info = ldap_get_entries($this->ldap_conn, $this->search_result);
+    		if (isset($info[0]['givenname'][0]))
+    		{
+    			$user_fname = $info[0]['givenname'][0];
+    			return $user_fname;
+    		}
+    		
+    	}
+    }
+    public function getLastName()
+    {
+    	if (!empty($this->search_result))
+    	{
+    		$info = ldap_get_entries($this->ldap_conn, $this->search_result);
+    		if (isset($info[0]['sn'][0]))
+    		{
+        		$user_lname = $info[0]['sn'][0];
+    			return $user_lname;
+    		}
+    	}
+
+    }
+    public function getRollNo()
+    {
+    	if (!empty($this->search_result))
+    	{
+    		$info = ldap_get_entries($this->ldap_conn, $this->search_result);
+    		if(isset($info[0]['employeenumber'][0]))
+    		{
+	    		$rollno = $info[0]['employeenumber'][0];
+	    		return $rollno;
+	    	}
+    	}
+    }
+
+    public function getMail()
+    {
+    	if (!empty($this->search_result))
+    	{
+    		$info = ldap_get_entries($this->ldap_conn, $this->search_result);
+    		if(isset($info[0]['mail'][0]))
+    		{
+        		$mail = $info[0]['mail'][0];
+    			return $mail;
+    		}
+    	}
+
+    }
+    public function getDept()
+    {
+    	if (!empty($this->search_result))
+    	{
+    		$info = ldap_get_entries($this->ldap_conn, $this->search_result);
+    		if(isset($info[0]['homedirectory'][0]))
+    		{
+    			$dept = getDept($info[0]['homedirectory'][0]);
+    			return $dept;
+    		}
+    	}
+    }
+    public function getSearchResult()
+    {
+    	return $this->search_result;
+    }
+    public function getInfo()
+    {
+    	return $this->info;
+    }
+    public function bind()
+    {
+    	if(!empty($this->search_result))
+    	{
+    		$info = ldap_get_entries($this->ldap_conn, $this->search_result);
+    		$user_dn = $info[0]['dn'];
+    		if(isset($this->pass))
+    		{
+    			$bind_result = @ldap_bind($this->ldap_conn, $this->user_dn, $this->pass);
+    			return true;
+    		}
+    	}
+    }
+
+}
+
+
+
+?>
